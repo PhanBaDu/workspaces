2bfc6943e34e892e1b5b1ca0edc6f8f66239dae6
Phan Ba Du <duphan03@gmail.com>
2025-04-18 01:58:07 +0700

    feat(workspaces): add workspace creation feature with form and API integration
    This commit introduces a new workspace creation feature, including a form component, API hooks, server routes, and validation schemas. The feature allows users to create workspaces with a name and optional image, which is uploaded and stored in the Appwrite database. The form is integrated with React Hook Form and Zod for validation, and the API handles the creation process, including image upload and workspace data storage. Additionally, the dashboard page has been updated to display the workspace creation form.

---
+++ src/features/workspaces/server/route.ts
@@ -0,0 +1,40 @@
+import { DATABASE_ID, IMAGE_BUCKET_ID, WORKSPACES_ID } from '@/config';
+import { createWorkspaceSchema } from '@/features/workspaces/schemas';
+import { sessionMiddleware } from '@/lib/session-middleware';
+import { zValidator } from '@hono/zod-validator';
+import { Hono } from 'hono';
+import { ID } from 'node-appwrite';
+
+const app = new Hono().post(
+    '/',
+    zValidator('form', createWorkspaceSchema),
+    sessionMiddleware,
+    async (c) => {
+        const databases = c.get('databases');
+        const storage = c.get('storage');
+        const user = c.get('user');
+
+        const { name, image } = c.req.valid('form');
+
+        let uploadedImageUrl = '';
+        if (image instanceof File) {
+            const file = await storage.createFile(IMAGE_BUCKET_ID, ID.unique(), image);
+
+            const arrayBuffer = await storage.getFileView(IMAGE_BUCKET_ID, file.$id);
+
+            uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString(
+                'base64',
+            )}`;
+        }
+
+        const workspace = await databases.createDocument(DATABASE_ID, WORKSPACES_ID, ID.unique(), {
+            userId: user.$id,
+            name,
+            imageUrl: uploadedImageUrl,
+        });
+
+        return c.json({ data: workspace });
+    },
+);
+
+export default app;

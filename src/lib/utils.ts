94c281b807060031b17b80925788d5b1551fc3cc
Phan Ba Du <duphan03@gmail.com>
2025-04-19 01:18:59 +0700

    feat(members): add member roles, invite code generation, and workspace association
    Introduce MemberRole enum to define member roles, add generateInviteCode utility for workspace invites, and update workspace route to associate members with workspaces. Also, add MEMBERS_ID to config for database integration.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
+}
+
+export function generateInviteCode(length: number) {
+    const charaters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz012456789';
+    let result = '';
+
+    for (let i = 0; i < length; i++) {
+        result += charaters.charAt(Math.floor(Math.random() * charaters.length));
+    }
+
+    return result;
 }

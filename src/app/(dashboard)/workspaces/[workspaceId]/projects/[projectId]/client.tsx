e6ada11bc2e937538671235d33be9efb65b7935f
Phan Ba Du <duphan03@gmail.com>
2025-04-22 13:15:31 +0700

    feat(analytics): add project analytics feature with task metrics
    Introduce a new analytics feature to display task-related metrics for projects. This includes total tasks, assigned tasks, completed tasks, overdue tasks, and incomplete tasks, along with their monthly differences. The feature is implemented with a new API endpoint, custom hooks, and UI components to visualize the data.

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
@@ -9,32 +9,40 @@ import { useProjectId } from '@/features/projects/hooks/use-project-id';
 import { useGetProject } from '@/features/projects/api/use-get-project';
 import PageLoader from '@/components/page-loader';
 import PageError from '@/components/page-error';
+import { useGetProjectAnalytics } from '@/features/projects/api/use-get-project-analytics';
+import Analytics from '@/components/analytics';
 
 export const ProjectIdClient = () => {
     const projectId = useProjectId();
-    const { data, isLoading } = useGetProject({
+    const { data: project, isLoading: isLoadingProject } = useGetProject({
         projectId: projectId,
     });
+    const { data: analytics, isLoading: isLoadingAnalytics } =
+        useGetProjectAnalytics({
+            projectId,
+        });
+
+    const isLoading = isLoadingProject || isLoadingAnalytics;
 
     if (isLoading) return <PageLoader />;
 
-    if (!data) return <PageError message="Project not found" />;
+    if (!project) return <PageError message="Project not found" />;
 
     return (
         <div className="flex flex-col gap-y-4">
             <div className="flex items-center justify-between">
                 <div className="flex items-center gap-x-2">
                     <ProjectAvatar
-                        name={data.name}
-                        image={data.imageUrl}
+                        name={project.name}
+                        image={project.imageUrl}
                         className="size-8"
                     />
-                    <p className="text-lg font-semibold">{data.name}</p>
+                    <p className="text-lg font-semibold">{project.name}</p>
                 </div>
                 <div>
                     <Button variant={'secondary'} size={'sm'} asChild>
                         <Link
-                            href={`/workspaces/${data.workspaceId}/projects/${data.$id}/settings`}
+                            href={`/workspaces/${project.workspaceId}/projects/${project.$id}/settings`}
                         >
                             <PencilIcon className="size-4 mr-2" />
                             Edit Project
@@ -42,6 +50,7 @@ export const ProjectIdClient = () => {
                     </Button>
                 </div>
             </div>
+            {analytics ? <Analytics data={analytics} /> : null}
             <TaskViewSwitcher hideProjectFilter />
         </div>
     );
---
+++ src/components/analytics.tsx
@@ -0,0 +1,65 @@
+import AnalyticsCard from '@/components/analytics-card';
+import { DashedSeparator } from '@/components/dashed-separator';
+import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
+import { ProjectAnalyticsResponseType } from '@/features/projects/api/use-get-project-analytics';
+
+export default function Analytics({ data }: ProjectAnalyticsResponseType) {
+    return (
+        <ScrollArea className="border rounded-lg w-full whitespace-nowrap shrink-0">
+            <div className="w-full flex flex-row">
+                <div className="flex items-center flex-1">
+                    <AnalyticsCard
+                        title="Total tasks"
+                        value={data.taskCount}
+                        variant={data.taskDifference > 0 ? 'up' : 'down'}
+                        increaseValue={data.taskDifference}
+                    />
+                    <DashedSeparator direction="vertical" />
+                </div>
+                <div className="flex items-center flex-1">
+                    <AnalyticsCard
+                        title="Assigned Tasks"
+                        value={data.assignedTaskCount}
+                        variant={
+                            data.assignedTaskDifference > 0 ? 'up' : 'down'
+                        }
+                        increaseValue={data.assignedTaskDifference}
+                    />
+                    <DashedSeparator direction="vertical" />
+                </div>
+                <div className="flex items-center flex-1">
+                    <AnalyticsCard
+                        title="Completed Tasks"
+                        value={data.completedTaskCount}
+                        variant={
+                            data.completedTaskDifference > 0 ? 'up' : 'down'
+                        }
+                        increaseValue={data.completedTaskDifference}
+                    />
+                    <DashedSeparator direction="vertical" />
+                </div>
+                <div className="flex items-center flex-1">
+                    <AnalyticsCard
+                        title="Overdue Tasks"
+                        value={data.overdueTaskCount}
+                        variant={data.overdueTaskDifference > 0 ? 'up' : 'down'}
+                        increaseValue={data.overdueTaskDifference}
+                    />
+                    <DashedSeparator direction="vertical" />
+                </div>
+                <div className="flex items-center flex-1">
+                    <AnalyticsCard
+                        title="Incomplete Tasks"
+                        value={data.incompleteTaskCount}
+                        variant={
+                            data.incompleteTaskDifference > 0 ? 'up' : 'down'
+                        }
+                        increaseValue={data.incompleteTaskDifference}
+                    />
+                    <DashedSeparator direction="vertical" />
+                </div>
+            </div>
+            <ScrollBar orientation="horizontal" />
+        </ScrollArea>
+    );
+}

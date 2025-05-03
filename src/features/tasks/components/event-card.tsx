230a81dcaf4239df2b1e5a37722890d65314747a
Phan Ba Du <duphan03@gmail.com>
2025-05-03 18:34:55 +0700

    style(event-card): truncate text to prevent overflow
    Added `truncate` class to the title and assignee section to prevent text overflow and improve readability. Also, added `overflow-hidden` to the container to ensure consistent styling.

--- src/features/tasks/components/event-card.tsx
+++ src/features/tasks/components/event-card.tsx
@@ -37,18 +37,17 @@ export default function EventCard({
         e.stopPropagation();
         router.push(`/workspaces/${workspaceId}/tasks/${id}`);
     };
-
     return (
         <div className="px-2">
             <div
                 onClick={onClick}
                 className={cn(
-                    'p-1.5 text-xs bg-background text-primary border rounded-md border-l-4 flex flex-col gap-y-1.5 cursor-pointer hover:opacity-75 transition',
+                    'p-1.5 text-xs bg-background overflow-hidden text-primary border rounded-md border-l-4 flex flex-col gap-y-1.5 cursor-pointer hover:opacity-75 transition',
                     statusColorMap[status],
                 )}
             >
-                <p className="text-primary">{title}</p>
-                <div className="flex items-center gap-x-1">
+                <p className="text-primary truncate ">{title}</p>
+                <div className="flex items-center gap-x-1 truncate">
                     <MemberAvatar name={assignee?.name} />
                     <div className="size-1 rounded-full bg-muted-foreground" />
                     <ProjectAvatar
